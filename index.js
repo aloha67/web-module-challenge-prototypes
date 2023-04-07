b0bd3ad6e20ff9eb37e926471e3f381deaa9c227
John Andrade <johnandrade117@yahoo.com>
2023-04-06 17:50:52 -0700

    Finished Tasks

--- index.js
+++ index.js
@@ -15,11 +15,29 @@
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
-
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+  this.stomach = [];
+}
+Person.prototype.eat = function(edible){
+  if(this.stomach.length < 10){
+    this.stomach.push(edible);
+  }
+}
+Person.prototype.poop = function(){
+  this.stomach = [];
+}
+Person.prototype.toString = function(){
+  return `${this.name}, ${this.age}`;
 }
 
-
+const josh = new Person('josh, 25');
+console.log(josh.toString());
+josh.eat('pizza');
+console.log(josh.poop);
+josh.poop();
+console.log(josh.stomach);
 /*
   TASK 2
     - Write a Car constructor that initializes `model` and `milesPerGallon` from arguments.
@@ -36,8 +54,14 @@ function Person() {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
-
+function Car(model, mpg) {
+  this.model =model;
+  this.milesPerGallon = mpg;
+  this.tank = 0;
+  this.odometer = 0;
+}
+Car.prototype.fill = function(gallons){
+  this.tank = this.tank + gallons;
 }
 
 
@@ -49,18 +73,22 @@ function Car() {
         + Should return a string "Playing with x", x being the favorite toy.
 */
 
-function Baby() {
-
+function Baby(name, age, favoriteToy) {
+  Person.call(this, name, age);
+  this.favoriteToy = favoriteToy;
 }
-
+Baby.prototype = Object.create(Person.prototype);
+Baby.prototype.play = function(){
+  return `Playing with ${this.favoriteToy}`;
+}
 
 /* 
   TASK 4
   In your own words explain the four principles for the "this" keyword below:
-  1. 
-  2. 
-  3. 
-  4. 
+  1. Explicit binding = .call, .apply, and .bind  
+  2. Implicit Binding = When used, look to what is on the left side of the dot to see what it is refering to
+  3. Window binding = Returns window or global object in node or undefined in strict mode
+  4. New Binding = Constructor function that points to new object
 */
 
 ///////// END OF CHALLENGE /////////
